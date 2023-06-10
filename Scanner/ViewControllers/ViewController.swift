9841a5acd66b424136df801fc28e254a5052742c
shadowSparrow <romanenko.aleksandre@gmail.com>
2023-06-11 01:57:52 +0200

    tectViewReflectsFullText()

--- Scanner/ViewControllers/ViewController.swift
+++ Scanner/ViewControllers/ViewController.swift
@@ -26,6 +26,7 @@ class ViewController: UIViewController {
         setViewConstraints()
         setTextViewContraints()
         setButtonContraints()
+        
     }
     
     @objc func startScanningAction(_ sender: UIButton) {
@@ -45,33 +46,35 @@ extension ViewController: VNDocumentCameraViewControllerDelegate {
         self.view.addSubview(addScanButton)
         
         scanTexView = UITextView()
-        scanTexView.backgroundColor = .cyan
+        //scanTexView.backgroundColor = .cyan
+        scanTexView.textColor = .white
         scanTexView.translatesAutoresizingMaskIntoConstraints = false
         self.view.addSubview(scanTexView)
         
         scanView = UIImageView()
-        scanView.backgroundColor = .blue
+        scanView.contentMode = .scaleAspectFit
+        //scanView.backgroundColor = .blue
         scanView.translatesAutoresizingMaskIntoConstraints = false
         self.view.addSubview(scanView)
         
-
+        
     }
     
     
     func setButtonContraints() {
         NSLayoutConstraint.activate([addScanButton.centerXAnchor.constraint(equalToSystemSpacingAfter: self.view.centerXAnchor , multiplier: 0),
-                                      addScanButton.heightAnchor.constraint(equalToConstant: 50),
-                                      addScanButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 16),
-                                      addScanButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -16),
-                                      addScanButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -100)])
+                                     addScanButton.heightAnchor.constraint(equalToConstant: 50),
+                                     addScanButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 16),
+                                     addScanButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -16),
+                                     addScanButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -100)])
     }
     
-     private func setTextViewContraints() {
-         NSLayoutConstraint.activate([scanTexView.heightAnchor.constraint(equalToConstant: 200),
-                                      scanTexView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 16),
-                                      scanTexView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -16),
-                                      scanTexView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-                                      scanTexView.bottomAnchor.constraint(equalTo: addScanButton.topAnchor, constant: -16)])
+    private func setTextViewContraints() {
+        NSLayoutConstraint.activate([scanTexView.heightAnchor.constraint(equalToConstant: 200),
+                                     scanTexView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 16),
+                                     scanTexView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -16),
+                                     scanTexView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+                                     scanTexView.bottomAnchor.constraint(equalTo: addScanButton.topAnchor, constant: -16)])
     }
     
     private func setViewConstraints() {
@@ -82,13 +85,13 @@ extension ViewController: VNDocumentCameraViewControllerDelegate {
     }
     
     
-   private func configureScanDocs() {
+    private func configureScanDocs() {
         let scanDoucument = VNDocumentCameraViewController()
         scanDoucument.delegate = self
         self.present(scanDoucument, animated: true)
     }
     
- func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFinishWith scan: VNDocumentCameraScan) {
+    func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFinishWith scan: VNDocumentCameraScan) {
         for pageNumber in 0..<scan.pageCount {
             let originalImage = scan.imageOfPage(at: pageNumber)
             detectText(in: originalImage)
@@ -113,7 +116,7 @@ extension ViewController: VNDocumentCameraViewControllerDelegate {
             }
         }
         request.recognitionLanguages = ["en_US"]
-        request.recognitionLevel = .accurate
+        request.recognitionLevel = .fast
         performDetection(request: request, image: image)
     }
     
@@ -133,25 +136,44 @@ extension ViewController: VNDocumentCameraViewControllerDelegate {
     }
     
     func handleDetectionResults(results: [Any]?) {
-      guard let results = results, results.count > 0 else {
-          print("No text found")
-          return
-      }
-
-      for result in results {
-          if let observation = result as? VNRecognizedTextObservation {
-              
-              for text in observation.topCandidates(1) {
-                  print(text.string)
-                  
-                  DispatchQueue.main.async {
-                      self.scanTexView.text = text.string
-                  }
-              }
-          }
-      }
+        guard let results = results, results.count > 0 else {
+            print("No text found")
+            return
+        }
+        
+        guard let obsetvations = results as? [VNRecognizedTextObservation] else {return}
+        
+        let text = obsetvations.compactMap({
+            $0.topCandidates(1).first?.string
+        }).joined(separator: " ")
+        
+        DispatchQueue.main.async {
+            self.scanTexView.text = text
+        }
+        
+        /*
+        for result in results {
+            if let observation = result as? VNRecognizedTextObservation {
+                
+                
+                
+                for text in observation.topCandidates(1)[0...] {
+                   
+                    DispatchQueue.main.async {
+                        self.scanTexView.text = text.string
+                        print(text.string)
+                                     print(text.confidence)
+                                     print(observation.boundingBox)
+                                     print("\n")
+                    }
+                    
+                }
+        
+            }
+        }
+        
+        */
     }
+    
 }
-
-
 
